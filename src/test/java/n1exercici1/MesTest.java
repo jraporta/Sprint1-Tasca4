fe4b3c8499d38ecd8fda88eea92384b4ff2dbe50
jraporta <jraporta@gmail.com>
2024-07-29 15:42:42 +0200

    correccions

--- src/test/java/n1exercici1/MesTest.java
+++ src/test/java/n1exercici1/MesTest.java
@@ -4,17 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MesTest {
+	
+	private List<String> llistaMesos;
 
+	@BeforeEach
+	public void creaLlistaMesos() {
+		llistaMesos = Mes.creaLlistaMesos();
+	}
+	
+	
 	@Test
 	@DisplayName(value = "El métode creaLlistaMesos ha de retornar una llista de tamany 12")
 	public void CorrectSizeTest() {
-		
-		List<String> llistaMesos = Mes.creaLlistaMesos();
-		
+	
 		assertEquals(llistaMesos.size(), 12);
 	}
 	
@@ -22,16 +29,12 @@ public class MesTest {
 	@DisplayName(value = "creaLlistaMesos retorna una llista no buida")
 	public void ListIsNotEmptyTest() {
 		
-		List<String> llistaMesos = Mes.creaLlistaMesos();
-		
 		assertFalse(llistaMesos.isEmpty());	
 	}
 	
 	@Test
 	@DisplayName(value = "verificar que agost està situat a la posició 7ª")
 	public void CheckAugustPositionTest() {
-		
-		List<String> llistaMesos = Mes.creaLlistaMesos();
 		
 		assertEquals(llistaMesos.get(7), "agost");	
 	}
--- src/test/java/n3exercici5/AppTest.java
+++ src/test/java/n3exercici5/AppTest.java
@@ -4,8 +4,6 @@ package n3exercici5;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
-
 import java.util.HashMap;
 import java.util.Map;
 
